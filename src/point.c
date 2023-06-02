15714c48b65d198667f6afa9a65c6a88419d7110
= <valentin.mouret@hey.com>
2023-06-02 20:19:09 +0200

    Add shorthand syntax for struct initialization

--- src/point.c
+++ src/point.c
@@ -1,19 +1,26 @@
-#include <stdio.h>
 #include <math.h>
+#include <stdio.h>
 
 // This syntax is used to name struct types.
-typedef struct
-{
-    int x;
-    int y;
+typedef struct {
+  int x;
+  int y;
 } Point;
 
-Point new_point(int x, int y)
-{
-    Point p;
-    p.x = x;
-    p.y = y;
-    return p;
+Point new_point(int x, int y) {
+  Point p;
+  p.x = x;
+  p.y = y;
+  return p;
+}
+
+// There is a shorthand syntax to instantiate structs.
+Point new_point_2(int x, int y) {
+  Point p = {
+      x = x,
+      y = y,
+  };
+  return p;
 }
 
 /**
@@ -22,20 +29,15 @@ Point new_point(int x, int y)
  * @param p2 The second point.
  * @return The distance between the two points.
  */
-float distance(Point p1, Point p2)
-{
-    return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
+float distance(Point p1, Point p2) {
+  return sqrt(pow(p1.x - p2.x, 2) + pow(p1.y - p2.y, 2));
 }
 
-void print_point(Point p)
-{
-    printf("(%d, %d)\n", p.x, p.y);
-}
+void print_point(Point p) { printf("(%d, %d)\n", p.x, p.y); }
 
-int main(int argc, char *argv[])
-{
-    Point p = new_point(1, 2);
-    print_point(p);
-    printf("Distance: %f\n", distance(p, new_point(3, 4)));
-    return 0;
+int main(int argc, char *argv[]) {
+  Point p = new_point(1, 2);
+  print_point(p);
+  printf("Distance: %f\n", distance(p, new_point(3, 4)));
+  return 0;
 }
